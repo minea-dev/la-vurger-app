3e13b34d127804a2cc672033e75b583767b532df
Minea Licer <minealicer@gmail.com>
2026-03-02 17:59:55 +0100

    init: projecte base La Vurger (Spring Boot + Angular)

---
+++ backend/src/main/java/com/mlicer/uoc/lavurgerapi/LaVurgerApiApplication.java
@@ -0,0 +1,14 @@
+package com.mlicer.uoc.lavurgerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaVurgerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaVurgerApiApplication.class, args);
+    }
+}
+
+
---
+++ backend/src/test/java/com/mlicer/uoc/lavurgerapi/LaVurgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlicer.uoc.lavurgerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaVurgerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
